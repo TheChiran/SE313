4277cc30ce2621c16e59e14220faf91cac0cdf87
ChiranSWE <30518298+Thebrazen@users.noreply.github.com>
2020-02-22 11:23:27 +0600

    Update MergeSort.c (#1)
    very very bad

--- MergeSort.c
+++ MergeSort.c
@@ -4,6 +4,8 @@ class MergeSort
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
+    
+    //New Line Added
     void merge(int arr[], int l, int m, int r) 
     { 
         // Find sizes of two subarrays to be merged 
